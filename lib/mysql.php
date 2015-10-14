0776d269ed8c75ac32f63981f94630dfcf3307ce
syaffers <syafiqkamarulazman@gmail.com>
2015-10-14 21:33:29 +0400

    Removing old files, bug fixes

--- lib/mysql.php
+++
@@ -1,9 +0,0 @@
-function connect_db() {
-	$server = '127.0.0.1'; // this may be an ip address instead
-	$user = 'syaffers';
-	$pass = 'pizza';
-	$database = 'wstation';
-	$connection = new mysqli($server, $user, $pass, $database);
-
-	return $connection;
-}
