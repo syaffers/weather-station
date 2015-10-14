2a048f6f62c5d82c67f8be39bf21d379ff57173e
syaffers <syafiqkamarulazman@gmail.com>
2015-10-14 21:19:40 +0400

    Changed db login

--- lib/mysql.php
+++ lib/mysql.php
@@ -1,7 +1,7 @@
 function connect_db() {
 	$server = '127.0.0.1'; // this may be an ip address instead
-	$user = 'root';
-	$pass = 'TONYHAWK';
+	$user = 'syaffers';
+	$pass = 'pizza';
 	$database = 'wstation';
 	$connection = new mysqli($server, $user, $pass, $database);
 
